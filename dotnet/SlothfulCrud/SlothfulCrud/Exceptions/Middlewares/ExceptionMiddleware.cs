407c1519081178aedd09c544eb001e26807c20f7
dfyda <damian.fyda@gmail.com>
2024-04-21 11:32:58 +0200

    Added exception middleware.

---
+++ dotnet/SlothfulCrud/SlothfulCrud/Exceptions/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using SlothfulCrud.Exceptions.Handlers;
+
+namespace SlothfulCrud.Exceptions.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IExceptionHandler _exceptionHandler;
+
+        public ExceptionMiddleware(
+            RequestDelegate next,
+            IExceptionHandler exceptionHandler)
+        {
+            _next = next;
+            _exceptionHandler = exceptionHandler;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                await ProcessExceptionAsync(context, exception);
+            }
+        }
+
+        private Task ProcessExceptionAsync(HttpContext context, Exception exception)
+        {
+            var response = _exceptionHandler.HandleError(exception);
+
+            return CompleteAsync(context, response);
+        }
+
+        private Task CompleteAsync(HttpContext context, SlothProblemDetails problemDetails)
+        {
+            context.Response.ContentType = "application/problem+json";
+            context.Response.StatusCode = problemDetails.Status ?? StatusCodes.Status500InternalServerError;
+
+            return context.Response.WriteAsync(JsonSerializer.Serialize(problemDetails));
+        }
+    }
+}
