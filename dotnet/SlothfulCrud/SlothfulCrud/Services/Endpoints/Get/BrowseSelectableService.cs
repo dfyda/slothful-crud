3fff754e840e338b783742a016615f4eecbb6147
dfyda <damian.fyda@gmail.com>
2024-05-01 08:28:57 +0200

    Implemented base service structure.

--- dotnet/SlothfulCrud/SlothfulCrud/Services/Endpoints/Get/BrowseSelectableService.cs
+++ dotnet/SlothfulCrud/SlothfulCrud/Services/Endpoints/Get/BrowseSelectableService.cs
@@ -21,8 +21,43 @@ namespace SlothfulCrud.Services.Endpoints.Get
         
         public PagedResults<BaseEntityDto> Browse(ushort page, dynamic query)
         {
-            // TODO: Implement this method
+            var baseQuery = DbContext.Set<T>().AsQueryable();
+            var properties = query.GetType().GetProperties();
+
+            var resultQuery = FilterQuery(query, properties, baseQuery) as IQueryable<T>;
+            resultQuery = SortQuery(query, resultQuery);
+
+            resultQuery = resultQuery
+                .Take((int)query.Rows)
+                .Skip((int)query.Skip);
+
+            var data = GetBaseEntities(resultQuery);
+            var total = baseQuery.Count();
+            
+            return new PagedResults<BaseEntityDto>(query.Skip, total, page, data.ToList());
+        }
+
+        private IQueryable<T> SortQuery(dynamic query, IQueryable<T> queryObject)
+        {
             throw new NotImplementedException();
+        }
+
+        private IQueryable<T> FilterQuery(dynamic query, dynamic properties, IQueryable<T> queryObject)
+        {
+            throw new NotImplementedException();
+        }
+        
+        private IEnumerable<BaseEntityDto> GetBaseEntities(IQueryable<T> resultQuery)
+        {
+            var items = resultQuery.ToList();
+            foreach (var item in items)
+            {
+                yield return new BaseEntityDto
+                {
+                    Id = item.Id,
+                    DisplayName = item.DisplayName
+                };
+            }
         }
     }
 }
